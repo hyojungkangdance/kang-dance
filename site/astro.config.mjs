ed16215849b6b241a4e040ab3b2e1d547434f616
hyojungkangdance <postmaster@hyojungkang.dance>
2026-07-28 09:17:28 +0900

    astro seitenerstellt

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://kang.dance",
+
+  output: "static",
+
+  trailingSlash: "never",
+
+  build: {
+    format: "file",
+  },
+});
